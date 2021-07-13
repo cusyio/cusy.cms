74dc779eca740034f45891464b7f0ef131417636
Thomas Massmann <thomas.massmann@it-spir.it>
2021-07-13 10:43:25 +0200

    feat: hide required add-ons from quick installer

--- src/cusy/cms/setuphandlers.py
+++ src/cusy/cms/setuphandlers.py
@@ -11,6 +11,17 @@ class HiddenProfiles(object):
             "cusy.cms:uninstall",
         ]
 
+    def getNonInstallableProducts(self):
+        """Hide other products from site-creation and quickinstaller."""
+        return [
+            "collective.easyform",
+            "cusy.exportimport",
+            "cusy.restapi.easyform",
+            "cusy.restapi.info",
+            "cusy.restapi.patches",
+            "plone.restapi",
+        ]
+
 
 def post_install(context):
     """Post install script"""
