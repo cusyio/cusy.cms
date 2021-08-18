758b5ec2c4c4e13c5331b775629e4b9d40aa57ce
Thomas Massmann <thomas.massmann@it-spir.it>
2021-08-18 13:49:43 -0400

    feat(ISSUE-1): :sparkles: set banner scale to banner

---
+++ src/cusy/cms/tests/test_settings_banner.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""Test banner settings."""
+
+from cusy.cms import testing
+
+import plone.api
+import unittest
+
+
+class TestBannerSettings(unittest.TestCase):
+    """Validate banner settings."""
+
+    layer = testing.INTEGRATION_TESTING
+
+    def setUp(self):
+        """Additional test setup."""
+        self.portal = self.layer["portal"]
+
+    def test_banner_scale(self):
+        """Validate the 'banner_scale' setting."""
+        setting = plone.api.portal.get_registry_record(
+            "collective.behavior.banner.browser.controlpanel.IBannerSettingsSchema.banner_scale",
+        )
+        self.assertEqual(setting, "banner")
